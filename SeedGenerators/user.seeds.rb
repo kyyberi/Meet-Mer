4b58b794287c6bce27ab8343eff7d9891f4180c9
Jarkko (kyyberi) Moilanen <jarkko.moilanen@want3d.fi>
2012-07-16 22:22:06 +0300

    Adder user.seeds.rb file to ease setup

---
+++ SeedGenerators/user.seeds.rb
@@ -0,0 +1,6 @@
+puts 'SETTING UP DEFAULT USER LOGIN'
+user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
+puts 'New user created: ' << user.name
+user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please'
+puts 'New user created: ' << user2.name
+user.add_role :admin
