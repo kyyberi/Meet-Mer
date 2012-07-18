c7b13473523c5c02dd253cfc7f49dc05ef204a0c
yleis <jarkko.moilanen@want3d.fi>
2012-07-18 10:57:42 +0300

    Fixed migration rules for packages + some other minor issues

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 gem 'rails', '3.2.1'
 gem 'sqlite3'
+gem 'mysql2'
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
--- db/migrate/20120713215117_create_meetings.rb
+++ db/migrate/20120713215117_create_meetings.rb
@@ -1,13 +1,12 @@
 class CreateMeetings < ActiveRecord::Migration
-drop_table "meetings"
-  def change
+  def update
     create_table :meetings do |t|
       t.string :title
       t.string :startTime
       t.string :endTime
       t.text :content
       t.string :logsurl
-      t.string :url
+      t.string :murl
       t.string :channel
       t.datetime :meetingdate
       t.string :meetingyear
